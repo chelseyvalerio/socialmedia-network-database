8fd01923acc2bee273fd85d425362327904ca537
chelseyvalerio <cvalerio@csog.net>
2023-05-07 17:34:24 -0600

    packag.json updated and seeds added

---
+++ seeds/seeds.js
@@ -0,0 +1,24 @@
+// const sequelize = require('../config/connection');
+const { User, Thought } = require('../models');
+
+const userData = require('./userData.json');
+const thoughtData = require('./thoughtData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+    await Thought.bulkCreate(thoughtData,{
+      individualHooks: true,
+      returning: true,
+    });
+
+
+  process.exit(0);
+};
+
+seedDatabase();
